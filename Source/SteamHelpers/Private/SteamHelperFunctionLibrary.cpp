013f8120d5552058c67a7d331d0e8663aa410daa
Claire <40319009+larch13@users.noreply.github.com>
2026-06-20 14:27:16 -0500

    Steam Helper Function Library

--- Source/SteamHelpers/Private/SteamHelperFunctionLibrary.cpp
+++ Source/SteamHelpers/Private/SteamHelperFunctionLibrary.cpp
@@ -2,4 +2,34 @@
 
 
 #include "SteamHelperFunctionLibrary.h"
+#include "ThirdParty/Steamworks/sdk/public/steam/steam_api.h"
 
+bool USteamHelperFunctionLibrary::IsDLCInstalled(int32 AppID)
+{
+	if (SteamUser() == nullptr)
+	{
+		return false;
+	}
+
+	return SteamApps()->BIsDlcInstalled(AppID);
+}
+
+bool USteamHelperFunctionLibrary::IsAppInstalled(int32 AppID)
+{
+	if (SteamUser() == nullptr)
+	{
+		return false;
+	}
+
+	return SteamApps()->BIsAppInstalled(AppID);
+}
+
+void USteamHelperFunctionLibrary::OpenStore(int32 DLC_AppID)
+{
+	if (SteamUser() == nullptr)
+	{
+		return;
+	}
+
+	SteamFriends()->ActivateGameOverlayToStore(DLC_AppID, k_EOverlayToStoreFlag_None);
+}
--- Source/SteamHelpers/Public/SteamHelperFunctionLibrary.h
+++ Source/SteamHelpers/Public/SteamHelperFunctionLibrary.h
@@ -13,5 +13,14 @@ UCLASS()
 class STEAMHELPERS_API USteamHelperFunctionLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
+public:
 	
+	UFUNCTION(BlueprintCallable, META = (DisplayName = "Has DLC Installed", CompactNodeTitle = "Has DLC Installed", Category = "Steam Helper Functions"))
+	static bool IsDLCInstalled(int32 AppID);
+
+	UFUNCTION(BlueprintCallable, META = (DisplayName = "Game Installed", CompactNodeTitle = "Game Installed", Category = "Steam Helper Functions"))
+	static bool IsAppInstalled(int32 AppID);
+
+	UFUNCTION(BlueprintCallable, META = (DisplayName = "Open Store", CompactNodeTitle = "Open Store", Category = "Steam Helper Functions"))
+	static void OpenStore(int32 DLC_AppID);
 };
